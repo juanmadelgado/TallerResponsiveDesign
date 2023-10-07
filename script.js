112d4822df7fc5e5c6907b6a16f85940e55b4a88
juanmadelgado <juanmadel2002@gmail.com>
2023-10-07 18:01:49 -0300

    carousel options-responsive

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const menu_options = document.getElementById("menu-options");
+
+function showOptions(){
+
+    if (menu_options.classList.contains("d-none")){
+        menu_options.classList.remove("d-none");
+    }else{
+        menu_options.classList.add("d-none");
+    }
+
+}
+   
+
+
+   
+
+   
+   
